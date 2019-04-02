c0e9f8dc1f640282686df32766df865aaa9f910e
akhilramani <ramaniakhil094@gmail.com>
2019-04-02 11:25:12 +0530

    oauthLogin styling applied

--- src/components/auth/oauthLogin.component.js
+++ src/components/auth/oauthLogin.component.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {GoogleLogin} from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
+import {TiSocialFacebookCircular} from 'react-icons/ti'; 
 
 const serverUrl = `http://${process.env.GRAPH_QL_HTTP_SERVER_HOST}:${process.env.GRAPH_QL_HTTP_SERVER_PORT}`
 
 class OauthLogin extends Component {
 
     googleResponse = (response) => {
-        axios.post(`${serverUrl}/auth/google`, {access_token: response.accessToken})
+        axios.post(`${serverUrl}/auth/google`, { access_token: response.accessToken })
             .then((res) => {
                 console.log(res.data)
                 this.props.onSuccess(res.data);
@@ -17,7 +18,7 @@ class OauthLogin extends Component {
     };
 
     facebookResponse = (response) => {
-        axios.post(`${serverUrl}/auth/facebook`, {access_token: response.accessToken})
+        axios.post(`${serverUrl}/auth/facebook`, { access_token: response.accessToken })
             .then((res) => {
                 console.log(res.data);
                 this.props.onSuccess(res.data);
@@ -31,18 +32,23 @@ class OauthLogin extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{ textAlign: 'center', padding: 25, paddingTop: 0 }}>
+            <hr style={{margin: '30px 25% 30px 25%'}}/>
                 <GoogleLogin
                     clientId={process.env.GOOGLE_CLIENT_ID}
-                    buttonText="login with Google"
+                    buttonText="Login with Google"
                     onSuccess={this.googleResponse}
                     onFailure={this.onFailure}
                 />
-                <FacebookLogin 
+                <br />
+                <div style={{height: 18}} ></div>
+                <FacebookLogin
                     appId={process.env.FACEBOOK_CLIENT_ID}
                     buttonText='login with Facebook'
                     fields="name,email,picture"
                     callback={this.facebookResponse}
+                    cssClass="facebook-btn"
+                    icon={<TiSocialFacebookCircular size={27} color="#4c68b9" style={{paddingRight: 13}} />}
                 />
             </div>
         );
